b994c28035cbbf5f2b30ac914706fda31ea07751
darynet <darynet.com@gmail.com>
2019-06-21 17:24:41 +0400

    Update parser_lst.py
    + интерфейс

--- parser_lst.py
+++ parser_lst.py
@@ -2,65 +2,81 @@ import psycopg2
 import pandas as pd
 import glob, os
 
-path = r'D:\Project\WorkRegistry\dataXLS\tmp' # путь где лежат эксельки
-all_files = glob.glob(path + "\*.xlsx")
 
-insertdata = []
-dataset = []
-fields = ['Дата','Исполнитель',	'Код', 'Наименование', 'Работы', 'Список контактов по работе', 'Затрачено времени (в минутах)', 'Состояние', 'Видимость', 'Закрытых заявок Ремеди', 'Контрагент', 'Вид затрат', 'Функциональный блок', 'Вид работ', 'Вид услуг СФ',	'Вид формирования СФ']
-for filename in all_files:
-    tz = pd.read_excel(filename, sheet_names='Первичное внесение данных', usecols=fields)
-    tz = tz.fillna('')
-    tz.loc[tz['Закрытых заявок Ремеди'] == '', 'Закрытых заявок Ремеди'] = '0'	# ClosedRemedyRequestCnt - int, пустое или null при записи в БД не прокатывает
-    tz['Дата'] = tz['Дата'].apply(str)	#
+import tkinter as tk
+from tkinter import filedialog
 
-    for row in tz.itertuples(index=True, name=None):
-        dataset.append(row)
 
-# очень тупо меняем индексы (потому что нормально через цикл у меня не получилось сделать)
-i=0
-for row in dataset:
-	row = list(row)
-	row[0] = i+1
-	i=i+1
-	insertdata.append(row)
-# print(insertdata)
-# коннектимся к БД
-conn = psycopg2.connect(host='localhost', dbname='work_registry', user='testuser', password='1')
-cursor = conn.cursor()
- 
-# insert = sql.SQL(
-for row in range(len(insertdata)):
-	cursor.execute(
-	   	"""do $body$
-	declare
-	  r_init int;
-	  r_post int;
-	  v__cnt_reg int;
-	  v__id_portion int;
-	  v__first_reg_id int;
-	begin
-	  select tzi__init() into r_init;
-	  if r_init <> 999 /* успешное завершение */ then
-	    raise exception 'Ошибка инициализации %%s', r_init;
+root= tk.Tk()
+
+canvas1 = tk.Canvas(root, width = 300, height = 300, bg = 'lightsteelblue')
+canvas1.pack()
+
+def getExcel ():
+	path = r'D:\Project\WorkRegistry\dataXLS\tmp' # путь где лежат эксельки
+	all_files = glob.glob(path + "\*.xlsx")
+
+	insertdata = []
+	dataset = []
+	fields = ['Дата','Исполнитель',	'Код', 'Наименование', 'Работы', 'Список контактов по работе', 'Затрачено времени (в минутах)', 'Состояние', 'Видимость', 'Закрытых заявок Ремеди', 'Контрагент', 'Вид затрат', 'Функциональный блок', 'Вид работ', 'Вид услуг СФ',	'Вид формирования СФ']
+	for filename in all_files:
+	    tz = pd.read_excel(filename, sheet_names='Первичное внесение данных', usecols=fields)
+	    tz = tz.fillna('')
+	    tz.loc[tz['Закрытых заявок Ремеди'] == '', 'Закрытых заявок Ремеди'] = '0'	# ClosedRemedyRequestCnt - int, пустое или null при записи в БД не прокатывает
+	    tz['Дата'] = tz['Дата'].apply(str)	#
+
+	    for row in tz.itertuples(index=True, name=None):
+	        dataset.append(row)
+
+	# очень тупо меняем индексы (потому что нормально через цикл у меня не получилось сделать)
+	i=0
+	for row in dataset:
+		row = list(row)
+		row[0] = i+1
+		i=i+1
+		insertdata.append(row)
+	# print(insertdata)
+	# коннектимся к БД
+	conn = psycopg2.connect(host='localhost', dbname='work_registry', user='testuser', password='1')
+	cursor = conn.cursor()
+	 
+	# insert = sql.SQL(
+	for row in range(len(insertdata)):
+		cursor.execute(
+		   	"""do $body$
+		declare
+		  r_init int;
+		  r_post int;
+		  v__cnt_reg int;
+		  v__id_portion int;
+		  v__first_reg_id int;
+		begin
+		  select tzi__init() into r_init;
+		  if r_init <> 999 /* успешное завершение */ then
+		    raise exception 'Ошибка инициализации %%s', r_init;
+		  end if;
+
+		  insert into Data4Registry (rown, WorkDT, Executor, WorkCode, WorkName, WorkDescr, Party, TimeCosts, StateInfo, VisibilityName, ClosedRemedyRequestCnt, CostKindInfo, CustomerInfo, FunctionBlockInfo, WorkKindInfo, SFServKindInfo, SFMakeKindInfo)
+		values (%s, %s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
+		;
+
+	  select tzi__post() into r_post;
+	  if r_post <> 999 /* успешное завершение */ then
+	    raise exception 'Ошибка при записи данных %%', r_post;
+	  else
+	    select id_portion, first_reg_id, cnt_reg from DataPostInfo into v__id_portion, v__first_reg_id, v__cnt_reg;
+	    RAISE INFO 'id_portion = %%, first_reg_id = %%, cnt_reg = %%', v__id_portion, v__first_reg_id, v__cnt_reg;
 	  end if;
 
-	  insert into Data4Registry (rown, WorkDT, Executor, WorkCode, WorkName, WorkDescr, Party, TimeCosts, StateInfo, VisibilityName, ClosedRemedyRequestCnt, CostKindInfo, CustomerInfo, FunctionBlockInfo, WorkKindInfo, SFServKindInfo, SFMakeKindInfo)
-	values (%s, %s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s,%s)
-	;
+		end;
+		$body$ language plpgsql;
+		""", insertdata[row])
 
-  select tzi__post() into r_post;
-  if r_post <> 999 /* успешное завершение */ then
-    raise exception 'Ошибка при записи данных %%', r_post;
-  else
-    select id_portion, first_reg_id, cnt_reg from DataPostInfo into v__id_portion, v__first_reg_id, v__cnt_reg;
-    RAISE INFO 'id_portion = %%, first_reg_id = %%, cnt_reg = %%', v__id_portion, v__first_reg_id, v__cnt_reg;
-  end if;
+	cursor.close() # Закрываем подключение (rollback)
+	# conn.commit()
+	conn.close()
 
-	end;
-	$body$ language plpgsql;
-	""", insertdata[row])
+browseButton_Excel = tk.Button(text='Загрузить ТЗ', command=getExcel, bg='green', fg='white', font=('helvetica', 12, 'bold'))
+canvas1.create_window(150, 150, window=browseButton_Excel)
 
-cursor.close() # Закрываем подключение (rollback)
-# conn.commit()
-conn.close()
+root.mainloop()
